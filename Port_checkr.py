e197f18c784a30c4792248876a2938663655bf72
a-asaker <39923238+a-asaker@users.noreply.github.com>
2019-06-15 14:18:54 -0400

    Update Port_checkr.py

--- Port_checkr.py
+++ Port_checkr.py
@@ -1,35 +1,75 @@
 #!/usr/bin/env python3
-# Coded By : A_Asaker
-
+#Coded By : A_Asaker
 import socket
-import sys
+import sys,os,time
 import threading
 from urllib.request import urlopen
+import ipaddress
 
+socket.setdefaulttimeout(5)
 server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 def usage():
 	print('''	| Port Forwarding Checker - By A_Asaker |
-	~ Usage : ./Port_checkr.py [ Port Number ]
-	~ Example : ./Port_checkr.py 214''')
+	~ Usage : ./Port_checkr.py [ host ] [ Port Number ]
+		- host : 
+			~ -l => local ip address, for this device.
+			~ -p or -r => public/remote ip address, for this device.
+			~ <ip address> => any other device ip address.
+		- ports : ~ if port is under 1024, then you must run it 
+			    with "sudo" [as a root].
+	~ Example : ./Port_checkr.py -l 214
+	~ Example : ./Port_checkr.py -r 214
+	~ Example : ./Port_checkr.py 231.45.2.67 214''')
 	sys.exit()
 
-if len(sys.argv)==1:
+if len(sys.argv)<3:
 	usage()
 else:
-	port=int(sys.argv[1])
+	port=int(sys.argv[2])
 
-host = urlopen('http://ip.42.pl/raw').read().decode('UTF-8')
+try:
+	myhost = urlopen('http://ip.42.pl/raw').read().decode('UTF-8')
+	no_conn=0
+except :
+	no_conn=1
 
+if sys.argv[1]=="-l":
+	host=socket.gethostbyname(socket.gethostname())
+	local=1
+elif sys.argv[1]=="-r" or sys.argv[1]=="-p":
+	if no_conn:
+		print(" ~ There Is No Internet Access. ")
+		sys.exit(0)
+	host = myhost
+	local=1
+else:
+	if no_conn:
+		print(" ~ There Is No Internet Access. ")
+		sys.exit(0)
+	host=sys.argv[1]
+	if(ipaddress.ip_address(host).is_private or host==myhost):
+		local=1
+	else:
+		local=0
+
+if port <1024 and local :
+	if not os.geteuid() == 0:
+		print(" ~ Ports Under 1024 Must Be Run As Root \"sudo\".")
+		sys.exit(0)
+
+established=0
 def serv_est():
+	global established
 	try:
 		server.bind(("0.0.0.0",port))
 		server.listen(1)
 		conn,addr=server.accept()
+		established=1
 	except OSError as e:
-		print(" [#] There May Be Some Other Applications Are Using Port {} !".format(port))
+		print(" [~] May Be  There Are Some Other Applications Are Using Port {} !".format(port))
 		sys.exit()
 	except Exception as e:
 		print(e)
@@ -38,16 +78,29 @@ def serv_est():
 def clnt_cnn():
 	try:
 		client.connect((host,port))
-		print(" [#] Port {} [ Is Forwarded ] For This Device!".format(port))
+		if(sys.argv[1]=="-l"):
+			print(" [#] Port {} [ Is Forwarded ] To This Device !".format(port))
+		elif(sys.argv[1]=="-r" or sys.argv[1]=="-p"):
+			time.sleep(.05)
+			if (established):
+				print(" [#] Port {} [ Is Forwarded ] On [{}] To This Device!".format(port,host))
+			else:
+				print(" [#] Port {} [ Is Forwarded ] On [{}] To Another Device!".format(port,host))
+		else:
+			if(local):
+				print(" [#] Port {} [ Is Forwarded ] To This Device !".format(port))
+			else:
+				print(" [#] Port {} [ Is Opened ] On [{}] !".format(port,host))
 	except ConnectionRefusedError as e:
-		print(" [#] Port {} [ Is Not Forwarded ] For This Device!".format(port))
+		print(" [X] Port {} [ Is Not Forwarded/Opened ] On [{}] !".format(port,host))
 		sys.exit()
 	except Exception as e:
-		print(" [#] Port {} [ Is Not Forwarded ] For This Device!".format(port))
+		print(" [X] Port {} [ Is Not Forwarded/Opened ] On [{}] !".format(port,host))
 		sys.exit()
 
 def main():
-	threading.Thread(target=serv_est,daemon=True).start()
+	if local:
+		threading.Thread(target=serv_est,daemon=True).start()
 	threading.Thread(target=clnt_cnn).start()
 
 if __name__ == '__main__':
